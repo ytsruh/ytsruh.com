cee51844bac20768d944783cc717a1dff6111428
Chris Hurst <ytsruh@gmail.com>
2022-02-21 20:50:22 +0000

    Fixed Contact & Added Pokemon mini app

---
+++ src/routes/apps/pokedex/about.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Nav from "../../../lib/pokedex/nav.svelte";
+</script>
+
+<div class="p-8 max-w-6xl mx-auto">
+  <Nav />
+  <h1 class="text-4xl text-center my-8 uppercase">About</h1>
+  <p class="text-center">This is a cool demo app that uses Svelte Kit to build a Pokedex web application.</p>
+</div>
