241357d0e018822ee2b09b526985fbafd64bdb4a
Chris Hurst <ytsruh@gmail.com>
2022-03-05 21:27:51 +0000

    Bug fixes & Google Tag Manager

--- src/routes/projects/[id].svelte
+++ src/routes/projects/[id].svelte
@@ -18,25 +18,26 @@
 <div class="container mt-32 flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full">
   <div class="w-full">
     <PageTitle title={project.name} intro={project.description} />
-    <div class="flex flex-row py-10">
-      <div class="basis-1/2 m-5">
-        <img src={`../${project.img}`} class="w-full bg-nav h-36 lg:h-72 object-cover" alt={project.name} />
+    <div class="flex flex-row flex-wrap py-10">
+      <div class="flex-none w-full md:basis-1/2 p-5">
+        <img src={`../${project.img}`} class="w-full bg-nav h-72 object-cover" alt={project.name} />
       </div>
-      <div class="basis-1/2 m-5">
+      <div class="flex-none w-full md:basis-1/2 p-5">
         <h2 class="text-white text-xl">Project Links</h2>
         <div class="my-5 flex flex-row">
           {#if project.link}
             <a
               href={project.link}
               target="_blank"
-              class="bg-theme text-white px-4 py-3 mx-2 rounded-md transition">View this project</a
+              class="bg-theme text-white text-center px-4 py-3 mx-2 rounded-md transition"
+              >View this project</a
             >
           {/if}
           {#if project.github}
             <a
               href={project.github}
               target="_blank"
-              class="bg-theme text-white px-4 py-3 mx-2 rounded-md transition">View on Github</a
+              class="bg-theme text-white text-center px-4 py-3 mx-2 rounded-md transition">View on Github</a
             >
           {/if}
         </div>
