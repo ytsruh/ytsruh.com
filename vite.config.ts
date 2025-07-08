50bf75347a5a51886a2b9731eaabbb4abd0c52d3
Chris Hurst <46472228+ytsruh@users.noreply.github.com>
2025-07-08 12:44:03 +0100

    feat: seup rr7 skeleton project

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite';
-import { sveltekit } from '@sveltejs/kit/vite';
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
